0da6a6cc9f260e3e389433ef6dd9b5ec5fb33738
Wei Zhong <zhongwei99@163.com>
2023-11-09 05:14:53 -0600

    Update options.lua

--- options.lua
+++ options.lua
@@ -2,6 +2,7 @@
 return {
   opt = {
     -- set to true or false etc.
+    shell = "/bin/zsh",
     relativenumber = true, -- sets vim.opt.relativenumber
     number = true, -- sets vim.opt.number
     spell = false, -- sets vim.opt.spell
