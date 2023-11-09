bf3859f83cf471320865a81c70b6c68464f8c9b3
Wei Zhong <zhongwei99@163.com>
2023-11-09 01:19:14 -0600

    Update community.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,15 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.feline-nvim" },
+  -- { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
+  -- { import = "astrocommunity.bars-and-lines.lualine-nvim" },
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.cmp-cmdline" },
+  -- { import = "astrocommunity.media.drop-nvim" },
+  -- { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
 }
